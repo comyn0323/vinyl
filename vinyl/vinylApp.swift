540fc95af7f612dd5938a52611b24855e17ca89a
윤형준 <coojjang0323@gmail.com>
2026-02-16 20:14:18 +0900

    Initial Commit

---
+++ vinyl/vinylApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinylApp.swift
+//  vinyl
+//
+//  Created by 윤형준 on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct vinylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
